5a589197ad7e1bab9178b6b1c542f2dfa23ad743
группы стационара <st@itstep.lan>
2019-11-14 20:03:27 +0300

    heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,17 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBMkPrtni1dNn-4TcUX_4MxR9eEtIIoRrU",
+    authDomain: "angular-blog-4f8fa.firebaseapp.com",
+    databaseURL: "https://angular-blog-4f8fa.firebaseio.com",
+    projectId: "angular-blog-4f8fa",
+    storageBucket: "angular-blog-4f8fa.appspot.com",
+    messagingSenderId: "784113798281",
+    appId: "1:784113798281:web:e102d15ee4d47f75cf0a58",
+    measurementId: "G-HFGDGG6R60"
+  },
+  admin: {
+    username: "admin",
+    password: "admin"
+  }
 };
